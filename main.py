792d1a0369e643cb9968cec129dc96c3e16db1f5
student <student@mail.ru>
2023-09-29 13:37:28 +0300

    Вариант 22.	Дописать метод для о нахождения количества книг, по цене в указанном диапазоне.

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ for line in lines:
 min_price = float(input("Введите минимальную цену: "))
 max_price = float(input("Введите максимальную цену: "))
 
+
+count_books = 0;
 for book in books:
     if min_price <= book[-1] <= max_price:
+        count_books += 1
         print(f"Книга: {book[0]}, Жанр: {book[1]}, Стоимость: {book[2]}")
+print("Количество книг: " + str(count_books))
